b765a6f235d9687bcd939547b0fbf220678361c6
Syauqi <sauki084@gmail.com>
2025-11-04 01:48:44 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// learn javascript
+console.log('Hello, World!');
+// This is a simple JavaScript program that prints a message to the console
+// It demonstrates the basic syntax of JavaScript
+// You can run this code in a web browser's console or in a Node.js environment
