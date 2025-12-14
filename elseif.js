581100c754198dd5094b0ad00181fca3649ea8da
Syauqi <sauki084@gmail.com>
2025-12-14 19:11:26 +0700

    Else If statement

---
+++ elseif.js
@@ -0,0 +1,20 @@
+const Hari = "Minggu";
+
+if (Hari === "Senin") {
+  console.log("Selamat bekerja!");
+} else if (Hari === "Minggu") {
+  console.log("Selamat berlibur!");
+}
+
+const nilai = 80;
+
+if (nilai < 60) {
+  console.log("Anda tidak lulus.");
+} else if (nilai >= 60 && nilai < 75) {
+  console.log("Nilai anda cukup.");
+} else if (nilai >= 75 && nilai < 90) {
+  console.log("Nilai anda baik");
+} else if (nilai > 90);
+{
+  console.log("Nilai anda sangat baik!");
+}
