d5ddd3954be4a2ea8cfbbc660e9d1c7d5aff976f
Syauqi <sauki084@gmail.com>
2025-12-15 05:15:04 +0700

    Operator logic or,  not, and switch statement

--- app-satu.js
+++ app-satu.js
@@ -1,20 +1,36 @@
-const password = prompt("Buat password minimal 6 karakter:");
+// const password = prompt("Buat password minimal 6 karakter:");
 
-if (password.length >= 6) {
-  console.log("Password valid.");
-} else {
-  console.log("Password minimal 6 karakter");
-}
+// if (password.length >= 6) {
+//   console.log("Password valid.");
+// } else {
+//   console.log("Password minimal 6 karakter");
+// }
 
-if (password.indexOf(" ") === -1) {
-  console.log("Password tidak mengandung spasi.");
-} else {
-  console.log("Password tidak boleh mengandung spasi.");
-}
+// if (password.indexOf(" ") === -1) {
+//   console.log("Password tidak mengandung spasi.");
+// } else {
+//   console.log("Password tidak boleh mengandung spasi.");
+// }
 
 // Operator Logic And
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-  console.log("Password valid");
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("Password valid");
+// } else {
+//   console.log("Password tidak memenuhi syarat");
+// }
+
+// Operator Logic Or
+// if (password.length >= 6 || password.indexOf(" ") === -1) {
+//   console.log("Password valid");
+// } else {
+//   console.log("Password tidak memenuhi syarat");
+// }
+
+// Operator Logic Not
+const role = prompt("Masukkan peran Anda (admin/user):");
+
+if (role !== "admin" && role !== "user") {
+  console.log("Peran tidak valid");
 } else {
-  console.log("Password tidak memenuhi syarat");
+  console.log("Peran valid");
 }
---
+++ switchstatement.js
@@ -0,0 +1,16 @@
+// Switch Statement
+const balon = prompt("Enter a color (red, blue, green):");
+
+switch (balon) {
+  case "red":
+    console.log("You chose red!");
+    break;
+  case "blue":
+    console.log("You chose blue!");
+    break;
+  case "green":
+    console.log("You chose green!");
+    break;
+  default:
+    console.log("Color not recognized.");
+}
