7660b1169c7f45772ac75d73652f459664cb3270
Syauqi <sauki084@gmail.com>
2025-12-23 04:46:30 +0700

    Nested loop

---
+++ nested-loop.js
@@ -0,0 +1,7 @@
+const pilihan = "abcd";
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i}. soal nomor ${1}:`);
+  for (let j = 0; j < pilihan.length; j++) {
+    console.log(`   pilihan ${pilihan[j]}`);
+  }
+}
