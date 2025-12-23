6f5cce8745fed6c68546bf1e1134e982c7df3354
Syauqi <sauki084@gmail.com>
2025-12-24 06:23:32 +0700

    Break

--- while.js
+++ while.js
@@ -4,9 +4,22 @@
 //   num++;
 // }
 
-const PASSWORD = "javascript";
-let guess = prompt("Masukkan password:");
-while (guess !== PASSWORD) {
-  guess = prompt("Password salah! Coba lagi:");
+// const PASSWORD = "javascript";
+// let guess = prompt("Masukkan password:");
+// while (guess !== PASSWORD) {
+//   guess = prompt("Password salah! Coba lagi:");
+// }
+// alert("Password benar! Anda berhasil masuk.");
+
+// let input = prompt("Masukkan sesuatu!");
+
+// while (true) {
+//   input = prompt(input);
+//   if (input.toLowerCase() === "stop") break;
+// }
+// alert("Program dihentikan!");
+
+for (let i = 0; i < 1000; i++) {
+  console.log(`Perulangan ke-${i}`);
+  if (i === 100) break;
 }
-alert("Password benar! Anda berhasil masuk.");
