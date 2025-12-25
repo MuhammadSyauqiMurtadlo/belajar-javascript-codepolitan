cc6c39bcecb23ab7b0efc234f33936dfd77f0213
Syauqi <sauki084@gmail.com>
2025-12-25 21:07:25 +0700

    Argument and parameter in function

---
+++ function.js
@@ -0,0 +1,20 @@
+// function lemparDadu() {
+//   console.log(Math.floor(Math.random() * 6) + 1);
+// }
+
+// function nyanyi() {
+//   console.log("La La La La La...");
+//   console.log("Ngapain sih kamu nyuruh aku nyanyi?");
+// }
+// nyanyi();
+// lemparDadu();
+
+// function selamatPagi(name) {
+//   console.log(`Selamat Pagi ${name}, semoga harimu menyenangkan!`);
+// }
+// selamatPagi("Andi");
+
+function jumlahkan(a = 0, b = 0) {
+  const hasil = a + b;
+  console.log(hasil);
+}
